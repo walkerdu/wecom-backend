008e8ce34f6d9546c66036e43d22f1e1783e6bee
walkerdu <walkerdualias@gmail.com>
2023-04-26 05:23:01 +0800

    optimize cache logic

--- internal/pkg/handler/text_message_handler.go
+++ internal/pkg/handler/text_message_handler.go
@@ -13,14 +13,21 @@ const WeChatTimeOutSecs = 5
 
 func init() {
 	handler := &TextMessageHandler{
-		rspMsgCache: make(map[string]string),
+		// 用户消息处理结果的cache，超过5s，就cache住, 等待用户指令进行推送
+		rspMsgCache: make(map[string]struct {
+			content string // cache的回复内容
+			msgId   int64  // cache此回复对应的request的message ID
+		}),
 	}
 
 	HandlerInst().RegisterLogicHandler(wechat.MessageTypeText, handler)
 }
 
 type TextMessageHandler struct {
-	rspMsgCache map[string]string // 用户消息处理结果的cache，超过5s，就cache住, 等待用户指令进行推送
+	rspMsgCache map[string]struct {
+		content string
+		msgId   int64
+	} // 用户消息处理结果的cache，超过5s，就cache住, 等待用户指令进行推送
 }
 
 func (t *TextMessageHandler) GetHandlerType() wechat.MessageType {
@@ -35,7 +42,9 @@ func (t *TextMessageHandler) HandleMessage(msg wechat.MessageIF) (wechat.Message
 		if cacheMsg, exist := t.rspMsgCache[textMsg.FromUserName]; !exist {
 			return &wechat.TextMessageRsp{Content: "nothing to continue"}, nil
 		} else {
-			return &wechat.TextMessageRsp{Content: cacheMsg}, nil
+			delete(t.rspMsgCache, textMsg.FromUserName)
+			log.Printf("[INFO][HandleMessage] cache Response send to user, MsgId=%d", cacheMsg.msgId)
+			return &wechat.TextMessageRsp{Content: cacheMsg.content}, nil
 		}
 	}
 
@@ -48,7 +57,13 @@ func (t *TextMessageHandler) HandleMessage(msg wechat.MessageIF) (wechat.Message
 
 	if time.Now().Unix()-begin >= WeChatTimeOutSecs {
 		log.Printf("[WARN][HandleMessage] Response cost time too long, cache it, MsgId=%d", textMsg.MsgId)
-		t.rspMsgCache[textMsg.FromUserName] = chatRsp
+		t.rspMsgCache[textMsg.FromUserName] = struct {
+			content string
+			msgId   int64
+		}{
+			content: chatRsp,
+			msgId:   textMsg.MsgId,
+		}
 	}
 
 	textMsgRsp := wechat.TextMessageRsp{
